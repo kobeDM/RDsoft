69724bbbda950b51e21cc23531e048682fb81cc7
Kentaro Miuchi <miuchi@panda.kobe-u.ac.jp>
2023-02-08 22:00:02 +0900

    backup.py

--- bin/RD-backup.py
+++ bin/RD-backup.py
@@ -2,9 +2,14 @@
 
 import subprocess, os,sys
 
+dirs_from=['/home/msgc/RD?']
+dirs_to=['/nadb/nadb41/msgc','/nadb/nadb57/msgc']
 
-#rsync -avz -e "ssh -p 22 -i ~/.ssh/id_rsa" /home/pi/adalm_data pi@03cDAQ.local:/mnt/lbghdd/radon_backup
+for dir_from in dirs_from:
+    for dir_to in dirs_to:
+        #bup_cmd=['rsync -avz',dir_from,dir_to]
+        bup_cmd='rsync -avz '+dir_from+" "+dir_to
+        print(bup_cmd)
+        subprocess.run(bup_cmd,shell="yes")
+    
 
-bup_cmd=['rsync -avz /home/msgc/RD* /nadb/nadb41/msgc','rsync -avz /home/msgc/RD* /nadb/nadb57/msgc']
-#print(bup_cmd)
-subprocess.run(bup_cmd,shell="yes")
