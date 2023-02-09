b0ec17025f5cd626edc03e1ef0f398cb6bc4d177
Kentaro Miuchi <miuchi@panda.kobe-u.ac.jp>
2023-02-09 10:28:41 +0900

    backup

--- bin/RD-backup.py
+++ bin/RD-backup.py
@@ -2,13 +2,13 @@
 
 import subprocess, os,sys
 
-dirs_from=['/home/msgc/RD?/']
+dirs_from=['/home/msgc/RD?']
 dirs_to=['/nadb/nadb41/msgc','/nadb/nadb57/msgc']
 
 for dir_from in dirs_from:
     for dir_to in dirs_to:
         #bup_cmd=['rsync -avz',dir_from,dir_to]
-        bup_cmd='rsync -avz '+dir_from+" "+dir_to
+        bup_cmd='rsync -avz --copy-links '+dir_from+" "+dir_to
         print(bup_cmd)
         subprocess.run(bup_cmd,shell="yes")
     
