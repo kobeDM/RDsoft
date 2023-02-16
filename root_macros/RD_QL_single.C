bdb549c77b687c432f14cc3738441f7eb9fede37
Kentaro Miuchi <miuchi@panda.kobe-u.ac.jp>
2023-02-16 17:31:03 +0900

    QLmacro

--- root_macros/RD_QL_single.C
+++ root_macros/RD_QL_single.C
@@ -1,24 +1,13 @@
 void RD_QL(){
 
-  TCanvas *cvs=new TCanvas("cvs","cvs",100,50,800,700);
+  TCanvas *cvs=new TCanvas("cvs","cvs",100,50,800,750);
   cvs->Divide(2,3);
 
   //input files
   const int file_num=2;
   string filename[file_num];
-  string filename_all[file_num];
-  string filename_head[file_num];
-  string filename_ext;
-  int file_id[file_num][2];
-  //filename[0]="/home/msgc/RD1/data/20230213/RD_0.dat";
-  //filename[1]="/home/msgc/RD3/data/20230213/RD_0.dat";
-  filename_head[0]="/home/msgc/RD1/data/20230213/RD_";
-  filename_head[1]="/home/msgc/RD3/data/20230213/RD_";
-  filename_ext="dat";
-  file_id[0][0]=0;//from
-  file_id[0][1]=69;//to
-  file_id[1][0]=0;
-  file_id[1][1]=51;
+  filename[0]="/home/msgc/RD1/data/20230213/RD_0.dat";
+  filename[1]="/home/msgc/RD3/data/20230213/RD_0.dat";
 
   //output
   //  string outputDir="test";
@@ -35,11 +24,8 @@ void RD_QL(){
   int tbin=200;
   int Ybin=100;
   int spbin=100;
-  double spmin=-100/1000.0;
+  double spmin=0/1000.0;
   double spmax=3000/1000.;
-  double spavmin=-100/1000.0;
-  double spavmax=100/1000.;
-
   int spMbin=100;
   double spMmin=0;
   double spMmax=10;
@@ -65,24 +51,22 @@ void RD_QL(){
   //histgrams
   string stWF[2],stSP[2],stSPM[2],stSPPo214[2],stSPPo218[2];
   string stWFs[2],stSPs[2],stSPMs[2],stSPPo214s[2],stSPPo218s[2];
-  TH1F *histSps[2],*histSps_nc[2],*histSps_avg[2],*histSps_MeV[2];
+  TH1F *histSps[2],*histSps_nc[2],*histSps_MeV[2];
   TH1F *histSps_Po214[2],*histSps_Po218[2];
   TH2F *histWFs[2];
-  for(int i=0;i<file_num;i++){ 
-    filename_all[i]=filename_head[i]+std::to_string(file_id[i][0])+"-"+std::to_string(file_id[i][1]);
-    stWF[i]="WF ("+filename_all[i]+")";
+  for(int i=0;i<file_num;i++){
+    stWF[i]="WF ("+filename[i]+")";
     stWFs[i]="WF"+to_string(i);
-    stSP[i]="voltage spectrum "+filename_all[i];
+    stSP[i]="voltage spectrum "+filename[i];
     stSPs[i]="voltage spectrum"+to_string(i);
-    stSPM[i]="energy spectrum"+filename_all[i];
+    stSPM[i]="energy spectrum"+filename[i];
     stSPMs[i]="energy spectrum"+to_string(i);
-    stSPPo214[i]="Po spectrum"+filename_all[i];
+    stSPPo214[i]="Po spectrum"+filename[i];
     stSPPo214s[i]="Po spectrum"+to_string(i);
-    stSPPo218[i]="Po spectrum"+filename_all[i];
+    stSPPo218[i]="Po spectrum"+filename[i];
     stSPPo218s[i]="Po spectrum"+to_string(i);
     histWFs[i]=new TH2F(stWFs[i].c_str(),stWF[i].c_str(),tbin,tmin,tmax,Ybin,Vmin,Vmax);
     histSps[i]=  new TH1F(stSPs[i].c_str(),stSP[i].c_str(),spbin,spmin,spmax);
-    histSps_avg[i]=  new TH1F(stSPs[i].c_str(),stSP[i].c_str(),spbin,spavmin,spavmax);
     histSps_nc[i]=  new TH1F(stSPs[i].c_str(),stSP[i].c_str(),spbin,spmin,spmax);
     histSps_MeV[i]=  new TH1F(stSPMs[i].c_str(),stSPM[i].c_str(),spMbin,spMmin,spMmax);    
     histSps_Po214[i]=  new TH1F(stSPMs[i].c_str(),stSPM[i].c_str(),spMbin,spMmin,spMmax); 
@@ -95,7 +79,6 @@ void RD_QL(){
     histWFs[i]->GetXaxis()->SetTitle("clock(10MHz)");
     histWFs[i]->GetYaxis()->SetTitle("V");
     histSps[i]->SetLineColor(2);
-    histSps_avg[i]->SetLineColor(1);
     histSps_nc[i]->SetLineColor(4);
     histSps_Po214[i]->SetFillColor(7);
     histSps_Po214[i]->SetFillStyle(3000);
@@ -118,8 +101,8 @@ void RD_QL(){
    int twin_avg[2];
    twin_avg[0]=400;
    twin_avg[1]=twin_avg[0]+twin;
-  double veto_neg=-50/1000.;
-  int utime;
+  int veto_neg=-100;
+  int utime[2];
   int start_time[2],end_time[2];
   double live_time[2];
   string line,dummy,livestr[2],Po214str[2],Po218str[2];
@@ -131,12 +114,9 @@ void RD_QL(){
   }
   //read
   for(int i=0; i< file_num; i++){
-  for(int j=file_id[i][0]; j< file_id[i][1]+1; j++){
-    //    filename[i]=filename_head[i]+"2"+"."+filename_ext;//std::to_string(j);    
-    filename[i]=filename_head[i]+std::to_string(j)+"."+filename_ext;//std::to_string(j);    
     evtID = 0;
-    cerr<<"reading "<<filename[i];
     std::ifstream ifs( filename[i] );
+    cerr<<"file name "<<i<<": "<<filename[i]<<endl;
     if( ifs.is_open() == false ) return 1;
     while( !ifs.eof( ) ) {
       line = "";
@@ -145,18 +125,16 @@ void RD_QL(){
 	//event head
 	//	 cerr<<line<<endl;
 	std::stringstream ss( line );
-	ss >> dummy >> dummy>>utime;
-	if(evtID==0&&j==file_id[i][0])start_time[i]=utime;
-	else end_time[i]=utime;
-	evtID++;
+	ss >> dummy >> dummy>>utime[i];
+	if(evtID==0)start_time[i]=utime[i];
+	else end_time[i]=utime[i];
       }
-      else if( line.length( ) <= 0 || strncmp( line.c_str( ), "#Wri", 4 ) == 0 ) {
+      else if( line.length( ) <= 0 || strncmp( line.c_str( ), "#", 1 ) == 0 ) {
 	//event end
 	if( evtID != 0 ) {
 	  if(veto[i]==0){
 	    //	    histSps[i]->Fill( ch1Max - ch1Min );
 	    histSps[i]->Fill( ch1Max - V_avg );
-	    histSps_avg[i]->Fill( V_avg );
 	    //	    E=cal_a[i]*(ch1Max - ch1Min)+cal_b[i];
 	    E=cal_a[i]*(ch1Max - V_avg)+cal_b[i];
 	    if(E>Eth_MeV){
@@ -179,13 +157,13 @@ void RD_QL(){
 	  //	  histSps_nc[i]->Fill( ch1Max - ch1Min );
 	  histSps_nc[i]->Fill( ch1Max - V_avg );
 	}
-	//++evtID;
+	++evtID;
 	clock = 0;
 	ch1Min = 1023;
 	ch1Max = -1024;
 	V_avg=0;
 	veto[i]=0;
-	//	if(evtID%100==0)std::cerr<<evtID<<"\r";
+	if(evtID%100==0)std::cerr<<evtID<<"\r";
 	continue;
       }
       std::stringstream ss( line );
@@ -202,17 +180,12 @@ void RD_QL(){
     }
     ifs.close();
     //file summary
-    //    cerr<<"file name "<<i<<": "<<filename[i]<<endl;
-    cerr<<" "<<evtID-1<<" evetns";
-    //    cerr<<" ended at "<<end_time[i]<<endl;
-    if(end_time[i]>start_time[i])live_time[i]=(end_time[i]-start_time[i])/3600./24;
-    //    cerr<<" cumalative measuremnt time: "<<live_time[i]<<" days"<<" ("<<end_time[i]-start_time[i]<<"sec)"<<endl;
-
-
-  }
-
+    //      cerr<<"file name "<<i<<": "<<filename[i]<<endl;
+    cerr<<"\n"<<evtID<<" evetns"<<endl;
     cerr<<" started at "<<start_time[i]<<endl;
-    cerr<<"toal measuremnt time: "<<live_time[i]<<" days"<<" ("<<end_time[i]-start_time[i]<<"sec)"<<endl;
+    cerr<<" ended at "<<end_time[i]<<endl;
+    if(end_time[i]>start_time[i])live_time[i]=(end_time[i]-start_time[i])/3600./24;
+    cerr<<"realtime: "<<live_time[i]<<" days"<<endl;
     //normalize
     histSps_MeV[i]->Sumw2(); 
     histSps_Po214[i]->Sumw2(); 
@@ -220,7 +193,7 @@ void RD_QL(){
     histSps_MeV[i]->Scale(1/live_time[i]/bin_MeV);
     histSps_Po214[i]->Scale(1/live_time[i]/bin_MeV);
     histSps_Po218[i]->Scale(1/live_time[i]/bin_MeV);
-  
+
   }
 
 
@@ -229,18 +202,16 @@ void RD_QL(){
    for(int i=0; i< file_num; i++){
      //     string livestr="livetime: "+std::to_string(live_time[1])+" days";
      //     livestr[i]="livetime: "+std::to_string(live_time[i])+" days";
-     std::stringstream livess,calass,calbss,cpd214ss,cpd218ss;
-     livess<< std::fixed<<std::setprecision(2) << live_time[i]; // 
-     calass<< std::fixed<<std::setprecision(3) << cal_a[i]; // 
-     calbss<< std::fixed<<std::setprecision(2) << cal_b[i]; //
-     cpd214ss<< std::fixed<<std::setprecision(2) <<  "("<<Po214_count[i]/live_time[i]<<" cpd)";
-     cpd218ss<< std::fixed<<std::setprecision(2) <<  "("<<Po218_count[i]/live_time[i]<<" cpd)";
+     std::stringstream livess,calass,calbss;
+     livess<< std::setprecision(2) << live_time[i]; // 
+     calass<< std::setprecision(3) << cal_a[i]; // 
+     calbss<< std::setprecision(2) << cal_b[i]; // 
      livestr[i]="livetime: "+livess.str()+" days";
-     calstr[i]="cal: "+calass.str()+"*V+"+calbss.str();
-     Po214str[i]="Po214 "+std::to_string(Po214_count[i])+" events "+cpd214ss.str(); 
-     Po218str[i]="Po218 "+std::to_string(Po218_count[i])+" events"+cpd218ss.str(); 
-     lt[i][0]=new TLatex(tmin+0.1*(tmax-tmin),-0.6*Vmax,filename_all[i].c_str());
-     lts[i][0]=new TLatex(spmin+0.1*(spmax-spmin),(histSps[i]->GetMaximum())*0.8,filename_all[i].c_str());
+     calstr[i]=calass.str()+"*mV+"+calbss.str();
+     Po214str[i]="Po214 "+std::to_string(Po214_count[i])+" events";
+     Po218str[i]="Po218 "+std::to_string(Po218_count[i])+" events";
+     lt[i][0]=new TLatex(tmin+0.1*(tmax-tmin),-0.6*Vmax,filename[i].c_str());
+     lts[i][0]=new TLatex(spmin+0.1*(spmax-spmin),(histSps[i]->GetMaximum())*0.8,filename[i].c_str());
      lts[i][1]=new TLatex(spmin+0.1*(spmax-spmin),(histSps[i]->GetMaximum())*0.7,livestr[i].c_str());
      lts[i][4]=new TLatex(spMmin+0.1*(spMmax-spMmin),(histSps_MeV[i]->GetMaximum())*0.9,livestr[i].c_str());
      lts[i][2]=new TLatex(spMmin+0.1*(spMmax-spMmin),(histSps_MeV[i]->GetMaximum())*0.8,Po214str[i].c_str());
@@ -261,7 +232,6 @@ void RD_QL(){
       //histSpCh1->Draw("same");
       histSps[i]->Draw();
       histSps_nc[i]->Draw("same");
-      histSps_avg[i]->Draw("same");
       histSps[i]->Draw("same");
       lts[i][0]->Draw();
       lts[i][1]->Draw();
